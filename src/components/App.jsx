5fd5d41ee3e9fe7695e966342e4c75427a6ff8fd
Volodymyr-Synhaievskyi <vovagidro@gmail.com>
2024-09-06 10:16:19 +0200

    added modal open/close functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
+import { Modal } from "./Modal";
+import { SignInForm } from "./SignInForm";
+// import { SignInForm } from "./SignInForm";
+
 export const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
         fontSize: 40,
-        color: '#010101'
+        color: "#010101",
       }}
     >
-      React homework template
+      <button type="button" onClick={() => setIsOpen(true)}>
+        Open modal
+      </button>
+      <Modal isOpen={isOpen} onClose={setIsOpen}>
+        <SignInForm />
+      </Modal>
     </div>
   );
 };
