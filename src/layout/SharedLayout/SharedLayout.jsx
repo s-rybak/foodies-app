e687f0a1ce1cc99478a695aa78a5c116d5ab42ff
Volodymyr-Synhaievskyi <vovagidro@gmail.com>
2024-09-09 12:14:08 +0200

    added sign in and sign up modals

--- src/layout/SharedLayout/SharedLayout.jsx
+++ src/layout/SharedLayout/SharedLayout.jsx
@@ -1,16 +1,19 @@
-import { Outlet } from 'react-router-dom';
-import styles from './SharedLayout.module.css';
-import { Header, Footer, Loader } from 'components';
-import { Suspense } from 'react';
+import { Outlet } from "react-router-dom";
+import styles from "./SharedLayout.module.css";
+import { Header, Footer, Loader } from "components";
+import { Suspense, useState } from "react";
+import CustomModal from "components/shared/CustomModal/CustomModal";
 
-const SharedLayout = () => (
-  <div className={styles.container}>
-    <Header />
-    <Suspense fallback={<Loader />}>
-      <Outlet />
-    </Suspense>
-    <Footer />
-  </div>
-);
+const SharedLayout = () => {
+  return (
+    <div className={styles.container}>
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+      <Footer />
+    </div>
+  );
+};
 
 export default SharedLayout;
