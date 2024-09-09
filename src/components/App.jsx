5f9c0dc4ececc7cd5a475a9ae824ec6abab96c02
Oleh Cheliakh <73850848+Oleh-Cheliakh@users.noreply.github.com>
2024-09-09 23:44:32 +0300

    Add receipe info

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,42 @@
-import SharedLayout from 'layout/SharedLayout/SharedLayout.jsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
+import SharedLayout from "layout/SharedLayout/SharedLayout.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy } from "react";
 
-const Home = lazy(() => import('pages/Home/Home.jsx'));
-const Category = lazy(() => import('pages/Category/Category.jsx'));
-const Recipe = lazy(() => import('pages/Recipe/Recipe.jsx'));
-const NotFound = lazy(() => import('pages/NotFound/NotFound.jsx'));
+const Home = lazy(() => import("pages/Home/Home.jsx"));
+const Category = lazy(() => import("pages/Category/Category.jsx"));
+const Recipe = lazy(() => import("pages/Recipe/Recipe.jsx"));
+const Profile = lazy(() => import("pages/Profile/Profile.jsx"));
+const NotFound = lazy(() => import("pages/NotFound/NotFound.jsx"));
 
 export const App = () => {
-  return (
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/recipe" element={<Recipe />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>      
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter basename="/">
+			<Routes>
+				<Route
+					path="/"
+					element={<SharedLayout />}>
+					<Route
+						index
+						element={<Home />}
+					/>
+					<Route
+						path="/category"
+						element={<Category />}
+					/>
+					<Route
+						path="/recipe/:recipeId"
+						element={<Recipe />}
+					/>
+					<Route
+						path="/user/:authorId"
+						element={<Profile />}
+					/>
+					<Route
+						path="*"
+						element={<NotFound />}
+					/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 };
