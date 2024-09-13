d4d88ee4f207fac18c1a4d15faf5064ec547f81e
Oleh Cheliakh <73850848+Oleh-Cheliakh@users.noreply.github.com>
2024-09-13 22:28:59 +0300

    Add conditional logic to button in Hero component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,19 +8,41 @@ const Category = lazy(() => import("pages/Category/Category.jsx"));
 const Recipe = lazy(() => import("pages/Recipe/Recipe.jsx"));
 const UserInfo = lazy(() => import("pages/Profile/UserInfo/UserInfo"));
 const NotFound = lazy(() => import("pages/NotFound/NotFound.jsx"));
+const AddRecipe = lazy(() => import("pages/AddRecipe/AddRecipe.jsx"));
 
 export const App = () => {
-  return (
-    <BrowserRouter basename="/foodies-app">
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/user/:id" element={<UserInfo/>} />
-          <Route path="/recipe/:recipeId" element={<Recipe />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter basename="/foodies-app">
+			<Routes>
+				<Route
+					path="/"
+					element={<SharedLayout />}>
+					<Route
+						index
+						element={<Home />}
+					/>
+					<Route
+						path="/category"
+						element={<Category />}
+					/>
+					<Route
+						path="/user/:id"
+						element={<UserInfo />}
+					/>
+					<Route
+						path="/recipe/add"
+						element={<AddRecipe />}
+					/>
+					<Route
+						path="/recipe/:recipeId"
+						element={<Recipe />}
+					/>
+					<Route
+						path="*"
+						element={<NotFound />}
+					/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 };
