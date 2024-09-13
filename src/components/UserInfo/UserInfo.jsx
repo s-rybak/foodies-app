89df58e119aa1b6ef85a5d92f7e55fe706a9d0b5
sgoithw <birch_deck_0s@icloud.com>
2024-09-13 21:27:17 +0200

    Added some fixes

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -69,35 +69,6 @@ const UserInfo = ({ user }) => {
     </div>
   );
 
-  return (
-      <div className={styles.wrapper}>
-        <div className={styles.userInfoContainer}>
-          <img
-              src={user.photo}
-          alt={`${user.name}`}
-          className={styles.userPhoto}
-        />
-        <div className={styles.userDetails}>
-          <h2 className={styles.userName}>{user.name}</h2>
-          <p className={styles.userEmail}>{user.email}</p>
-          <div className={styles.userStats}>
-            <p>
-              <strong>Recipes:</strong> {user.recipeCount}
-            </p>
-            <p>
-              <strong>Favorites:</strong> {user.favoriteCount}
-            </p>
-            <p>
-              <strong>Followers:</strong> {user.followersCount}
-            </p>
-            <p>
-              <strong>Following:</strong> {user.followingCount}
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default UserInfo;
