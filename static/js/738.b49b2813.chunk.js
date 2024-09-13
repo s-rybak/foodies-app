87296c2cf4a1db50055ca9bc40e1fa314fa37963
s-rybak <31307894+s-rybak@users.noreply.github.com>
2024-09-13 21:55:50 +0000

    Deploying to gh-pages from @ s-rybak/foodies-app@39ac9a7f005d723a6c6086ee7c62ffb9d84fbd64 🚀

--- static/js/738.b49b2813.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfoodies_app=self.webpackChunkfoodies_app||[]).push([[738],{1738:(e,a,s)=>{s.r(a),s.d(a,{default:()=>p});var d=s(579);function p(){return(0,d.jsx)("div",{children:"Category data"})}}}]);
-//# sourceMappingURL=738.b49b2813.chunk.js.map
