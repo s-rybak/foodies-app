b44f1254b315bdb04f3267feb7d84b7f24d5732a
BevzoI <bevzo.i.n@gmail.com>
2024-09-13 10:16:15 +0200

    The extra code was removed

---
+++ src/components/Profile/UserInfo/UserInfo.jsx
@@ -0,0 +1,45 @@
+import Image from "components/UI/Image/Image";
+import style from "./UserInfo.module.css";
+import avatar from "assets/img/avatar";
+import emptyImages from "assets/img/empty";
+import { SvgIcon } from "components/UI";
+
+export default function UserInfo({ userInfo, uploadAvatar = true }) {
+  const useAvatar = avatar[userInfo.avatar] ?? emptyImages["noImage"];
+  const avatarEmpty = avatar[userInfo.avatar] ? false : true;
+
+  return (
+    <div className={style["profile-head-holder"]}>
+      <div className={style["profile-head"]}>
+        <div className={style["profile-img-holder"]}>
+          <Image
+            src={useAvatar}
+            alt=""
+            className={`${style["user-avatar"]} ${
+              avatarEmpty ? style["no-image"] : ""
+            }`}
+          />
+          {uploadAvatar ?? (
+            <label className={style["upload-avatar"]}>
+              <input type="file" name="avatar" />
+              <SvgIcon name={"icon-plus"} />
+            </label>
+          )}
+        </div>
+        <h3 className={style["user-login"]}>Nadia</h3>
+        <ul className={style["profile-data"]}>
+          {userInfo.dataList.map((itemInfo, index) => (
+            <li className={style["profile-data-li"]} key={index}>
+              <span className={style["profile-data-li-name"]}>
+                {itemInfo["name"]}
+              </span>
+              <span className={style["profile-data-li-value"]}>
+                {itemInfo["value"]}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Profile/UserInfo/UserInfo.jsx
@@ -0,0 +1,38 @@
+import PathInfo from "components/PathInfo";
+import UserInfo from "components/Profile/UserInfo/UserInfo";
+import { Container, Title, UnderTitle } from "components/UI";
+
+export default function ProfileInfo() {
+  const userInfo = {
+    id: 1,
+    avatar: "user_1",
+    dataList: [
+      {
+        name: "Email:",
+        value: "nadia28682@gmai.com",
+      },
+      {
+        name: "Added recipes:",
+        value: 4,
+      },
+      {
+        name: "Followers:",
+        value: 5,
+      },
+    ],
+  };
+
+  const userLoginned = false;
+
+  return (
+    <Container>
+      <PathInfo currentPage="Profile" />
+      <Title>Profile</Title>
+      <UnderTitle maxWidth={442}>
+        Reveal your culinary art, share your favorite recipe and create
+        gastronomic masterpieces with us.
+      </UnderTitle>
+      <UserInfo userInfo={userInfo} uploadAvatar={userLoginned} />
+    </Container>
+  );
+}
