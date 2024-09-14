b7749852b2ea131bdc4d5fe377e519aa57c11ad3
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-09-14 04:57:50 +0200

    Partial fix header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -33,7 +33,9 @@ export default function Header() {
       >
         Foodies
       </NavLink>
+
       <HeaderNav isHome={isHome} />
+
       {isSignedIn ? (
         <HeaderProfile
           onClick={() => setModalLogoutOpen(true)}
