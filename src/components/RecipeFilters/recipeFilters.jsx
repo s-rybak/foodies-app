fa088ea376b76a922094771d6b87914268e37fc8
Nataliya Pustelnyk <nata_pustelnyk@ukr.net>
2024-09-15 13:21:39 +0200

    arrow

--- src/components/RecipeFilters/recipeFilters.jsx
+++ src/components/RecipeFilters/recipeFilters.jsx
@@ -1,12 +1,13 @@
-import {useDispatch, useSelector} from "react-redux";
-
+// recipeFilters.js
+import { useDispatch, useSelector } from "react-redux";
 import css from "./recipeFilters.module.css";
-import {selectIngredients} from "../../redux/ingredients/ingredientsSelectors";
-import {selectAreas} from "../../redux/areas/areasSelectors";
-import {useEffect} from "react";
-import {fetchIngredients} from "../../redux/ingredients/ingredientsOperations";
-import {fetchAreas} from "../../redux/areas/areasOperations";
+import { selectIngredients } from "../../redux/ingredients/ingredientsSelectors";
+import { selectAreas } from "../../redux/areas/areasSelectors";
+import { useEffect } from "react";
+import { fetchIngredients } from "../../redux/ingredients/ingredientsOperations";
+import { fetchAreas } from "../../redux/areas/areasOperations";
 
+// IngredientsFilters
 export const IngredientsFilters = ({ changeHandler }) => {
   const dispatch = useDispatch();
 
@@ -17,24 +18,27 @@ export const IngredientsFilters = ({ changeHandler }) => {
   const { ingredients } = useSelector(selectIngredients);
 
   return (
-    <select
-      name="ingredients"
-      className={css.select}
-      onChange={(e) => changeHandler(e.target.value)}
-      defaultValue=""
-    >
-      <option value="" disabled>
-        Please select an ingredient
-      </option>
-      {ingredients.map(({ id, name }) => (
-        <option key={id} value={id}>
-          {name}
+    <div className={css["select-wrapper"]}>
+      <select
+        name="ingredients"
+        className={css.select}
+        onChange={(e) => changeHandler(e.target.value)}
+        defaultValue=""
+      >
+        <option value="" disabled>
+          ingredient
         </option>
-      ))}
-    </select>
+        {ingredients.map(({ id, name }) => (
+          <option key={id} value={id}>
+            {name}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
+// AreaFilters
 export const AreaFilters = ({ changeHandler }) => {
   const dispatch = useDispatch();
 
@@ -45,33 +49,34 @@ export const AreaFilters = ({ changeHandler }) => {
   const { areas } = useSelector(selectAreas);
 
   return (
-    <select
-      name="areas"
-      className={css.select}
-      onChange={(e) => changeHandler(e.target.value)}
-      defaultValue=""
-    >
-      <option value="" disabled>
-        Please select an area
-      </option>
-      {areas.map(({ id, name }) => (
-        <option key={id} value={id}>
-          {name}
+    <div className={css["select-wrapper"]}>
+      <select
+        name="areas"
+        className={css.select}
+        onChange={(e) => changeHandler(e.target.value)}
+        defaultValue=""
+      >
+        <option value="" disabled>
+          area
         </option>
-      ))}
-    </select>
+        {areas.map(({ id, name }) => (
+          <option key={id} value={id}>
+            {name}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
+// RecipeFilters
 const RecipeFilters = ({ changeHandler }) => {
-
   const handleSelectChangeIngredient = (selectedId) => {
-    changeHandler(selectedId, 'ingredients');
-  }
-  const handleSelectChangeArea = (selectedId) => {
-    changeHandler(selectedId, 'areas');
+    changeHandler(selectedId, "ingredients");
   };
-
+  const handleSelectChangeArea = (selectedId) => {
+    changeHandler(selectedId, "areas");
+  };
 
   return (
     <div className={css.container}>
@@ -81,5 +86,5 @@ const RecipeFilters = ({ changeHandler }) => {
   );
 };
 
-
+// Додати експорт за замовчуванням для RecipeFilters
 export default RecipeFilters;
