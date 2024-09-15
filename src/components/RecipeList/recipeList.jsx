5f956b0893598866f615555a316bbb488da17b34
yevhenporohniaviy <porohniaviy.yevheni@gmail.com>
2024-09-15 22:02:56 +0300

    home page, favorites

--- src/components/RecipeList/recipeList.jsx
+++ src/components/RecipeList/recipeList.jsx
@@ -1,13 +1,31 @@
 import {RecipeCard} from "../RecipeCard/RecipeCard";
 import style from './recipeList.module.css';
+import {useEffect} from "react";
+import {getFavoriteRecipes} from "../../redux/recipes/recipesOperations";
+import {useDispatch, useSelector} from "react-redux";
+import {selectFavoriteRecipes} from "../../redux/recipes/recipesSelectors";
 
 const RecipeList = ({recipes}) => {
+	const dispatch = useDispatch();
+	const favoriteRecipes = useSelector(selectFavoriteRecipes);
+
+
+	useEffect(() => {
+		dispatch(getFavoriteRecipes());
+	}, [dispatch]);
+
+	const isFavorite = (id) => {
+		console.log(favoriteRecipes, id);
+		return false
+	}
+
+
 
   return (
 		<div className={style['recipes-list']}>
 			{
 				recipes.map((recipe) => (
-					<RecipeCard key={recipe.id} recipe={recipe}/>
+					<RecipeCard key={recipe.id} recipe={recipe} isFavorite={isFavorite(recipe.id)} />
 				))
 			}
 		</div>
